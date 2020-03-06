9eadcb497d257356fe63bb0761af6fdd0d407461
xtedor <santixcolmen2@gmail.com>
2020-03-06 16:30:53 -0500

    hoy

--- Pets/app/src/main/java/com/example/pets/LoginActivity.java
+++ Pets/app/src/main/java/com/example/pets/LoginActivity.java
@@ -3,6 +3,8 @@ package com.example.pets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void Btn_Login(View view){
+        Toast.makeText(this, "Has presionado login", Toast.LENGTH_SHORT).show();
     }
 }
--- Pets/app/src/main/java/com/example/pets/MainActivity.java
+++ Pets/app/src/main/java/com/example/pets/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.pets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.ViewAnimator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "App on device", Toast.LENGTH_LONG).show();
+    }
+    public void Btn_Login(View view){
+        Toast.makeText(this, "Has precionado login", Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Toast.makeText(this, "aplicacion ha comenzado", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "aplicacion en pantalla", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "aplicacion en pausa", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "aplicacion detenida", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "aplicacion se ha reiniciado", Toast.LENGTH_SHORT).show();
     }
 }
