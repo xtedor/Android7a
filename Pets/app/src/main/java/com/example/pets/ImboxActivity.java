7fa03fa51410f80adbfdf342509da2af9f88e5f7
xtedor <santixcolmen2@gmail.com>
2020-03-09 15:26:50 -0500

    9 marzo

---
+++ Pets/app/src/main/java/com/example/pets/ImboxActivity.java
@@ -0,0 +1,14 @@
+package com.example.pets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImboxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imbox);
+    }
+}
--- Pets/app/src/main/java/com/example/pets/LoginActivity.java
+++ Pets/app/src/main/java/com/example/pets/LoginActivity.java
@@ -2,7 +2,11 @@ package com.example.pets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.LiveFolders;
 import android.view.View;
 import android.widget.Toast;
 
@@ -14,6 +18,15 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
     public void Btn_Login(View view){
-        Toast.makeText(this, "Has presionado login", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "U've pressed login", Toast.LENGTH_SHORT).show();
+        Intent intent=new Intent(this,ImboxActivity.class);
+        startActivity(intent);
+
     }
+    public void Btn_Signup(View view){
+        Toast.makeText(this, "U've pressed Sing up", Toast.LENGTH_SHORT).show();
+        Intent intent=new Intent(this,RegisterActivity.class);
+        startActivity(intent);
+    }
+
 }
