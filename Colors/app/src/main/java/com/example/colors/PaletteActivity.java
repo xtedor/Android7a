58e9a84a32d9780784d50031e336f6342c206947
xtedor <santixcolmen2@gmail.com>
2020-03-30 15:33:09 -0500

    funciones menu

--- Colors/app/src/main/java/com/example/colors/PaletteActivity.java
+++ Colors/app/src/main/java/com/example/colors/PaletteActivity.java
@@ -1,8 +1,13 @@
 package com.example.colors;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class PaletteActivity extends AppCompatActivity {
 
@@ -10,5 +15,30 @@ public class PaletteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_palette);
+    }
+    //Metodo de moestrar menu en el celular
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu1, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //acciones del menu
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.iteTranparent:
+                Toast.makeText(this,"this text is going to change",Toast.LENGTH_SHORT);
+                break;
+            case R.id.iteSemiTrans:
+                break;
+
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
